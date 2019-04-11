95841261b4e32b869720954252b073b575041239
hedongyang <hedy@idoosmart.com>
2019-04-11 17:33:33 +0800

    修改page背景

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,7 +3,12 @@ Page({
     // text:"这是一个页面"
   },
   onLoad: function (options) {
-    wx.setNavigationBarTitle({ title: '抢红包' });  
+    wx.setNavigationBarTitle({ title: '抢红包' }); 
+    wx.showToast({
+      title:"加载中...",
+      icon: "loading",
+      duration:3000
+    })
   },
   onReady: function () {
     // 页面渲染完成
