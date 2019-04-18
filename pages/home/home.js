2b197e9b287649bf80f48f6e15a91919faa34a5c
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-04-18 23:22:29 +0800

    优化首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,7 @@ Page({
     items:[
       {
         icon:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555004603689&di=6161b038a8a7046bfe88e4d72e975729&imgtype=0&src=http%3A%2F%2Fwww.36588.com.cn%2FImageResourceMongo%2FUploadedFile%2Fdimension%2Fbig%2F7d10bc2b-db5b-4247-925c-0628d65b3f50.png",
-        name:"张三丰",
+       name:"张三丰",
        start_time:"2019-04-05 10:00:00",
        long_time:"60",
        count:"10",
