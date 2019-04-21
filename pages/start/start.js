8321191a121a3c8a957d16117f5e3d60328c4655
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-04-21 22:42:58 +0800

    完善首页和发布页

--- pages/start/start.js
+++ pages/start/start.js
@@ -1,6 +1,11 @@
 Page({
   data: {
-    // text:"这是一个页面"
+    items: [
+      {sentiment : "恭喜发财,大吉大利"},
+      {amount : "50"},
+      {count: 4 },
+      {mode: "开门大吉" }
+    ]
   },
   onLoad: function (options) {
     wx.setNavigationBarTitle({ title: '发红包' }); 
@@ -16,5 +21,6 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
-  }
+  },
+
 })
