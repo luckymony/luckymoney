a2677bb46ca20460c387c7a3bba7b13ca0d7ec8d
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-05-03 11:06:04 +0800

    完善编辑页面

--- component/playui/playui.js
+++ component/playui/playui.js
@@ -1,3 +1,17 @@
 Component({
+    data:{
+       times:["15分钟","30分钟","45分钟","60分钟","24小时"],
+       oneIndex: 1,  // 默认选中第几行
+    },
+    
+     timeChange: function (event) {
+      this.setData({
+        oneIndex: event.detail.value
+      });
+    },
+
+    cancel: function (event) {
+      console.log("取消了");
+    },
 
 });
