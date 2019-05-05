76e5693862211a41d97b51a8dec0ecb3379ab3aa
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-05-05 22:48:00 +0800

    完善红包发布页面

--- component/playui/playui.js
+++ component/playui/playui.js
@@ -1,17 +1,42 @@
 Component({
-    data:{
-       times:["15分钟","30分钟","45分钟","60分钟","24小时"],
-       oneIndex: 1,  // 默认选中第几行
-    },
-    
-     timeChange: function (event) {
-      this.setData({
-        oneIndex: event.detail.value
-      });
+  options: {
+    multipleSlots: true
+  },
+  properties:{
+    //活动限时
+    activityTime: {
+      type: String,
+      value: "24小时"
     },
 
-    cancel: function (event) {
-      console.log("取消了");
+    // 空红包个数
+    emptyMoneyCount: {
+      type: Number,
+      value: 0
     },
+
+    //机会个数
+    chanceCount: {
+      type: Number,
+      value: 1
+    },
+
+    //设置难度
+    difficulty: {
+      type: Array,
+      value: [{ "name": "简单", "level": 0, "selected": 0 },
+              { "name": "正常", "level": 1, "selected": 1 }, 
+              { "name": "困难", "level": 2, "selected": 0 }]
+    },
+  },
+  methods: {
+    difficultyTap: function (e) {
+      console.log(e.currentTarget.dataset.index);
+      let index = e.currentTarget.dataset.index;
+      this.data({
+        ['difficulty[index].selected']: 1,
+      });
+    },
+  },
 
 });
