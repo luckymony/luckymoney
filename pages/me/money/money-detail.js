6658ccd54eda841d82d0c81abc7e341ed1f831d1
zhouzj <zhouzhijin0908@126.com>
2019-05-15 14:30:44 +0800

    完善“红包记录”页面

--- pages/me/money/money-detail.js
+++ pages/me/money/money-detail.js
@@ -6,21 +6,103 @@ Page({
    */
   data: {
     send:false,
-    receive:true
+    receive:true,
+    money:{
+      money_total:666,
+      red_packet_num:68,
+      detail:[
+        { name:'经典玩法',
+          time:'0517 16:36',
+          money:"+66.00"
+        },
+        {
+          name: '财源滚滚',
+          time: '0518 12:06',
+          money: "+66.00"
+        },
+        {
+          name: '四季发财',
+          time: '0317 19:16',
+          money: "+66.00"
+        },
+      ]
+    }
+  },
+
+  testR:function(){
+    this.setData({
+      send: false,
+      receive: true,
+      money: {
+        money_total: -800,
+        red_packet_num: 123,
+        detail: [
+          {
+            name: '经典玩法',
+            time: '0517 16:36',
+            money: "+166.00"
+          },
+          {
+            name: '财源滚滚',
+            time: '0518 12:06',
+            money: "+9.00"
+          },
+          {
+            name: '四季发财',
+            time: '0317 19:16',
+            money: "+17.00"
+          },
+          {
+            name: '天女散花',
+            time: '0323 02:36',
+            money: "23.00"
+          },
+          {
+            name: '转转盘',
+            time: '0417 09:06',
+            money: "160.00"
+          }
+        ]
+      }
+    })
+  },
+
+  testS:function(){
+    this.setData({
+      send: true,
+      receive: false,
+      money: {
+        money_total: -100,
+        red_packet_num: 3,
+        detail: [
+          {
+            name: '天女散花',
+            time: '0323 02:36',
+            money: "-23.00"
+          },
+          {
+            name: '转转盘',
+            time: '0417 09:06',
+            money: "-160.00"
+          }
+        ]
+      }
+    })
   },
 
   select:function(e){
-    if ('w' == e.currentTarget.dataset.w){
-      this.setData({
-        send:false,
-        receive:true
-      })
-    }else if ('y' == e.currentTarget.dataset.y){
-      this.setData({
-        send: true,
-        receive: false
-      })
+    if ('r' == e.currentTarget.dataset.r){
+      this.testR()
+    }else if ('s' == e.currentTarget.dataset.s){
+      this.testS()
     }
+  },
+
+  /**
+ * 生命周期函数--监听页面加载
+ */
+  onLoad: function (options) {
+      this.testR()
   },
 
 
