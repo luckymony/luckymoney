5ae97001b30c10d62570f9bcc51f9c59868bfb95
550476128@qq.com <Hdy!13579>
2019-05-21 22:36:11 +0800

    完善玩法页面

--- pages/start/play/play.js
+++ pages/start/play/play.js
@@ -59,11 +59,18 @@
 Page({
 
   data: {
+    items: [],
     currentIndex: 0
   },
 
   onLoad: function (options) {
-
+    var obj = JSON.parse(options.value);
+    console.log(obj);
+    this.setData({
+      items: obj.types,
+      currentIndex: obj.index
+    });
+    wx.setNavigationBarTitle({ title: '红包玩法' });
   },
   /* 这里实现控制中间凸显图片的样式 */
   handleChange: function (e) {
