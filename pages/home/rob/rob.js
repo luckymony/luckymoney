1b87374e63738aaa55863f8fba72a6eaeecfeef7
550476128@qq.com <Hdy!13579>
2019-06-10 23:59:46 +0800

    优化首页

--- pages/home/rob/rob.js
+++ pages/home/rob/rob.js
@@ -15,7 +15,7 @@ Page({
       { icon: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555004603689&di=6161b038a8a7046bfe88e4d72e975729&imgtype=0&src=http%3A%2F%2Fwww.36588.com.cn%2FImageResourceMongo%2FUploadedFile%2Fdimension%2Fbig%2F7d10bc2b-db5b-4247-925c-0628d65b3f50.png", name: "张三丰", time: "08-08 10:00", money: "88.88"}],
     currentItems: [],
     boxHeight: 0,
-    showFoot:true
+    showLoading: false
   },
 
   showAllTap:function (e) {
@@ -38,8 +38,14 @@ Page({
   onLoad: function (options) {
     let res = wx.getSystemInfoSync();
     let height = res.windowHeight;
+    let newItems = [];
+    if (this.data.allItems.length > 5) {
+       newItems = this.data.allItems.slice(0, 5)
+    }else {
+      newItems = this.data.allItems
+    }
     this.setData({
-      currentItems: this.data.allItems.slice(0,5),
+      currentItems: newItems,
       boxHeight: height
     });
     wx.setNavigationBarTitle({ title: '开始抢红包' }); 
@@ -77,14 +83,22 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log("下啦加载");
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("上啦加载");
+    this.setData({
+      showLoading: true,
+    });
+    setTimeout(() => {
+      this.setData({
+        showLoading: false,
+      });
+    }, 2000);
   },
 
   /**
