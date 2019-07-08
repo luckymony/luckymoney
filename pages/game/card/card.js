c3e2603b386c8c6c95eea9eccd6f14cccc34034f
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-07-08 23:18:07 +0800

    完善卡片UI

--- pages/game/card/card.js
+++ pages/game/card/card.js
@@ -5,7 +5,7 @@ var windowHeight = wx.getSystemInfoSync().windowHeight;
 
 var redPacket = {
   w:(windowWidth - 90)/5,
-  h:(windowHeight - 260)/5,
+  h:(windowWidth - 90)/5*(5/4),
   c:"#ff00ff"
 };
 var redPackets = [];
@@ -31,15 +31,23 @@ Page({
     //使用wx.createContext获取绘图上下文context
     var x = 0;
     var y = 0;
+    var start_y = (windowHeight - (redPacket.h*5 + 60))/2;
+    //console.log(windowHeight);
     var context = wx.createContext();
     for(var i=0;i<25;i++) {
       x = 15 * ((i % 5) + 1) + redPacket.w * (i % 5);
-      y = 100 + (i / 5) * 15 + redPacket.h * (i / 5);
+      var index = parseInt(i / 5);
+      y = start_y + index * 15 + redPacket.h * index;
       context.setFillStyle(redPacket.c);
       context.beginPath();
       context.rect(x, y, redPacket.w, redPacket.h);
       context.closePath();
       context.fill();
+
+      context.setFontSize(16) //字体大小
+      context.setFillStyle('#fff') //字体颜色
+      context.textAlign = "center"; //文字居中
+      context.fillText("点击我",x+redPacket.w/2,y+redPacket.h/2);
     }
     wx.drawCanvas({
       canvasId: "flipCardCanvas",
