420c28fc4adf93995fa3901d32583c8aff0c6006
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-07-11 23:36:44 +0800

    调试移动卡片

--- pages/game/card/card.js
+++ pages/game/card/card.js
@@ -4,6 +4,8 @@ var windowWidth  = wx.getSystemInfoSync().windowWidth;
 var windowHeight = wx.getSystemInfoSync().windowHeight;
 
 var redPacket = {
+  x:0,
+  y:0,
   w:(windowWidth - 90)/5,
   h:(windowWidth - 90)/5,
   c:"#ff00ff"
@@ -26,6 +28,13 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+  /*
+      context.setFontSize(16) //字体大小
+      context.setFillStyle('#fff') //字体颜色
+      context.textAlign = "center"; //文字居中
+      context.fillText(i,x+redPacket.w/2,y+redPacket.h/2);
+  */
+
   onLoad: function (options) {
     wx.setNavigationBarTitle({ title: '开门大吉' }); 
     //使用wx.createContext获取绘图上下文context
@@ -35,28 +44,47 @@ Page({
     var start_x = (windowWidth - (redPacket.w*5 + 32))/2;
     //console.log(windowHeight);
     var context = wx.createContext();
+
+    function DrawRedPacket(redPacket) {
+      context.beginPath();
+      context.setFillStyle(redPacket.c);
+      context.rect(redPacket.x, redPacket.y, redPacket.w, redPacket.h);
+      context.closePath();
+      context.fill();
+    }
+
     for(var i=0;i<25;i++) {
       x = start_x + 8 * (i % 5) + redPacket.w * (i % 5);
       var index = parseInt(i / 5);
-      y = start_y + index * 15 + redPacket.h * index;
-      context.setFillStyle(redPacket.c);
-      context.beginPath();
-      context.rect(x, y, redPacket.w, redPacket.h);
-      context.closePath();
-      context.fill();
-
-      context.setFontSize(16) //字体大小
-      context.setFillStyle('#fff') //字体颜色
-      context.textAlign = "center"; //文字居中
-      context.fillText(i,x+redPacket.w/2,y+redPacket.h/2);
+      y = start_y + index * 8 + redPacket.h * index;
+      redPacket.x = x;
+      redPacket.y = y;
+      redPackets.push(redPacket);
+      DrawRedPacket(redPacket);
     }
+    console.log(redPackets[4]);
+    redPackets[4].x = 200;
+    redPackets[4].y = 300;
+    redPackets[4].c = "#ccc";
+    DrawRedPacket(redPackets[4]);
     wx.drawCanvas({
       canvasId: "flipCardCanvas",
       actions: context.getActions()
     });
+
+    
   },
 
-  
+  /** 移动方案1
+   * 4->10
+   * 6->4
+   * 7->15
+   * 8->21
+   * 9->8
+   * 10->13
+   * 15->6
+   * 21->7
+   */
 
   /**
    * 生命周期函数--监听页面初次渲染完成
