e64e991038b0945ab875933695838611be8571df
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-07-14 23:49:20 +0800

    实现简单移动动画

--- pages/game/card/card.js
+++ pages/game/card/card.js
@@ -2,8 +2,12 @@
 
 var windowWidth  = wx.getSystemInfoSync().windowWidth;
 var windowHeight = wx.getSystemInfoSync().windowHeight;
-
+var fromItems = [4];
+var toItems = [10];
+var mobileDistanceX = 0;
+var mobileDistanceY = 0;
 var redPacket = {
+  i:0,
   x:0,
   y:0,
   w:(windowWidth - 90)/5,
@@ -11,6 +15,7 @@ var redPacket = {
   c:"#ff00ff"
 };
 var redPackets = [];
+var movePackets = [];
 
 Page({
 
@@ -34,44 +39,124 @@ Page({
       context.textAlign = "center"; //文字居中
       context.fillText(i,x+redPacket.w/2,y+redPacket.h/2);
   */
+  
 
-  onLoad: function (options) {
-    wx.setNavigationBarTitle({ title: '开门大吉' }); 
+  drawRedPacket:function() {
     //使用wx.createContext获取绘图上下文context
+    // return;
     var x = 0;
     var y = 0;
-    var start_y = (windowHeight - (redPacket.h*5 + 60))/2;
-    var start_x = (windowWidth - (redPacket.w*5 + 32))/2;
+    var start_y = (windowHeight - (redPacket.h * 5 + 60)) / 2;
+    var start_x = (windowWidth - (redPacket.w * 5 + 32)) / 2;
+
     //console.log(windowHeight);
     var context = wx.createContext();
 
-    function DrawRedPacket(redPacket) {
+    function rand(min, max) {
+      return parseInt(Math.random() * (max - min) + min);
+    }
+    var allCount = rand(1, 25);
+    
+    function drawPacket(packet) {
       context.beginPath();
-      context.setFillStyle(redPacket.c);
-      context.rect(redPacket.x, redPacket.y, redPacket.w, redPacket.h);
+      context.setFillStyle(packet.c);
+      context.rect(packet.x, packet.y, packet.w, packet.h);
       context.closePath();
       context.fill();
     }
 
-    for(var i=0;i<25;i++) {
+    function isFromDraw(index) {
+      for (var i = 0; i < fromItems.length; i++) {
+        var obj = fromItems[i];
+        if (index == obj) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function isToDraw(index) {
+      for (var i = 0; i < toItems.length; i++) {
+        var obj = toItems[i];
+        if (index == obj) {
+          return true;
+        }
+      }
+      return false;
+    }
+    redPackets = [];
+    for (var i = 0; i < 25; i++) {
+      var packet = {};
       x = start_x + 8 * (i % 5) + redPacket.w * (i % 5);
       var index = parseInt(i / 5);
       y = start_y + index * 8 + redPacket.h * index;
-      redPacket.x = x;
-      redPacket.y = y;
-      redPackets.push(redPacket);
-      DrawRedPacket(redPacket);
+      packet.x = x;
+      packet.y = y;
+      packet.w = redPacket.w;
+      packet.h = redPacket.h;
+      packet.c = redPacket.c;
+      packet.i = i;
+      redPackets.push(packet);
+      if (!isFromDraw(i)) {
+        drawPacket(packet);
+      }
     }
-    console.log(redPackets[4]);
-    redPackets[4].x = 200;
-    redPackets[4].y = 300;
-    redPackets[4].c = "#ccc";
-    DrawRedPacket(redPackets[4]);
+    
+    var fromPacket = redPackets[4];
+    var toPacket   = redPackets[10];
+    var total_x = (fromPacket.x - toPacket.x);
+    var total_y = (toPacket.y - fromPacket.y);
+    var avg_x = total_x/100;
+    var avg_y = total_y/100;
+    mobileDistanceX += avg_x;
+    mobileDistanceY += avg_y;
+    // console.log(mobileDistanceX);
+    // console.log(mobileDistanceY);
+    if(fromPacket.x > toPacket.x) {
+      fromPacket.x -= mobileDistanceX;
+    }else {
+      fromPacket.x += mobileDistanceX;
+    }
+    if (fromPacket.y > toPacket.y) {
+      fromPacket.y -= mobileDistanceY;
+    } else {
+      fromPacket.y += mobileDistanceY;
+    }
+    fromPacket.c = "#eee";
+    // console.log(fromPacket);
+    drawPacket(fromPacket);
+    // // console.log(redPackets);
+    // for (var i = 0; i < toItems.length; i++) {
+    //   var index1 = toItems[i];
+    //   var index2 = fromItems[i];
+    //   var redPacket1 = redPackets[index1];
+    //   var redPacket2 = redPackets[index2];
+    //   var total_x = (redPacket1.x - redPacket2.x);
+    //   var total_y = (redPacket1.y - redPacket2.y);
+    //   var avg_x = total_x/10;
+    //   var avg_y = total_y/10;
+    //   //console.log(avg_x, avg_y);
+    //   redPacket2.x += avg_x;
+    //   redPacket2.y += avg_y;
+    //   redPacket2.c = "#eee";
+    //   drawPacket(redPacket2);
+    // }
+
+    // console.log(redPackets[4]);
+    // redPackets[4].x = 200;
+    // redPackets[4].y = 300;
+    // redPackets[4].c = "#ccc";
+    // DrawRedPacket(redPackets[4]);
     wx.drawCanvas({
       canvasId: "flipCardCanvas",
       actions: context.getActions()
     });
+   
+  },
 
+
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({ title: '开门大吉' }); 
     
   },
 
@@ -90,7 +175,8 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.drawRedPacket();
+    this.interval = setInterval(this.drawRedPacket,50)
   },
 
   /**
@@ -111,7 +197,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    clearInterval(this.interval)
   },
 
   /**
