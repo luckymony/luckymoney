e04bcc78533d411c7438494c5a0e4185b9802789
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-08-10 21:58:01 +0800

    完成红包动画

--- component/redpacket/redpacket.js
+++ component/redpacket/redpacket.js
@@ -4,7 +4,24 @@ Component({
    * 组件的属性列表
    */
   properties: {
-  
+    //活动限时
+     iconUrl: {
+      type: String,
+      value: "../../images/me/user_icon_default.png"
+    },
+     userName:{
+       type: String,
+       value: "张三丰"
+     },
+     message:{
+       type: String,
+       value: "恭喜发财,大吉大利"
+     },
+     money:{
+       type: Number,
+       value: 0.5
+     },
+
   },
   /**
    * 组件的初始数据
@@ -80,195 +97,269 @@ Component({
     ],
     closeImageSrc: "../../images/packet/icon_red_packet_close.png",
     coinHidden:false,
-    current_i : 0,
-    current_y : 100,
+    index : 0,
+    startY : 100,
     transparency : 0,
+    drawType:0,
     setInter:'',
-    currentTimeout1: '',
+    currentTimeout: '',
     windowWidth : wx.getSystemInfoSync().windowWidth,
     windowHeight : wx.getSystemInfoSync().windowHeight,
-    move_y1: wx.getSystemInfoSync().windowHeight * 0.08,
-    move_y2: wx.getSystemInfoSync().windowHeight * 0.4 + wx.getSystemInfoSync().windowHeight * 0.08,
+    moveY1: wx.getSystemInfoSync().windowHeight * 0.08,
+    moveY2: wx.getSystemInfoSync().windowHeight * 0.4 + wx.getSystemInfoSync().windowHeight * 0.08,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    _circleImg:function(ctx,img) {
+    
+    startOpenReadPacket:function() {
+      console.log('开始动画')
       var that = this;
-      var x = that.data.windowWidth / 2 - 15;
-      var y = 20;
-      var r = 15;
-      ctx.save();
-      var d = 2 * r;
-      var cx = x + r;
-      var cy = y + r;
-      ctx.arc(cx, cy, r, 0, 2 * Math.PI);
-      ctx.clip();
-      ctx.drawImage(img, x, y, d, d);
-      ctx.restore();
+      that._animation1();
+      that.currentTimeout = setTimeout(function (){
+        that.setData({
+          coinHidden:true
+        });
+        that._animation3();
+      },5000);
     },
 
-    _drawRedPacketBg:function(ctx,y) {
-      var that = this;      
-      ctx.beginPath();
-      ctx.setFillStyle("#EB5646");
-      ctx.lineTo(that.data.windowWidth, 0);
-      ctx.lineTo(that.data.windowWidth, y);
-      ctx.arc(that.data.windowWidth / 2, y, that.data.windowWidth / 2, 0, Math.PI, false);
-      ctx.lineTo(0, y);
-      ctx.lineTo(0, 0);
-      ctx.closePath();
-      ctx.fill();
+    openReadPacketSuccess:function() {
+      console.log('成功获得红包')
+      var that = this;
+      that._animation3();
     },
 
-    _drawRedPacketText:function(ctx,text,font,y,isMoney) {
+    openRedPakcetFailed:function() {
+      console.log('失败获得红包')
+    },
+
+    _animation1:function() {
       var that = this;
-      ctx.font = 'normal bold ' + font +'px sans-serif'
-      ctx.setFillStyle('#FDE1B1') //字体颜色
-      ctx.textAlign = "center"; //文字居中
-      var x = that.data.windowWidth / 2;
-      ctx.fillText(text,x,y);
-      if(isMoney) {
-        ctx.setFontSize(13) //字体大小
+      var scale = 0.1;
+      that.data.setInter = setInterval(
+        function () {
+          if (scale > 1) {
+            clearInterval(that.data.setInter);
+            that._animation2();
+          } else {
+            if (scale >= 1.0) scale = 1.0;
+            that._drawRedPacketStart(scale);
+            if (scale < 1) scale = scale + 0.2;
+          }
+        }, 50); 
+    },
+
+    _animation2:function() {
+      var that = this;
+      that.data.setInter = setInterval(
+        function () {
+          that.setData({
+            index: that.data.index >= 10 ? 0 : (that.data.index + 1),
+          });
+          that._drawRedPacketOpen();
+        }, 100); 
+    },
+
+    _animation3:function() {
+      var that = this;
+      that.data.setInter = setInterval(
+        function () {
+          that.setData({
+            moveY1: that.data.moveY1 - 60,
+            moveY2: that.data.moveY2 + 60,
+            transparency: that.data.transparency + 0.1
+          });
+          that._drawRedPacketEnd(that.data.startY);
+          if (that.data.moveY1 < - (that.data.windowHeight * 0.4 + 60)
+            && that.data.moveY2 > that.data.windowHeight + 60) {
+            clearInterval(that.data.setInter);
+            that.setData({
+              transparency: 1
+            });
+            that._drawRedPacketEnd(that.data.startY);
+          }
+        }, 100); 
+    },
+    
+    _drawRedPacketEnd:function (y) {
+      function circleImg(ctx,w,img) {
+        var that = this;
+        var x = w / 2 - 15;
+        var y = 20;
+        var r = 15;
+        ctx.save();
+        var d = 2 * r;
+        var cx = x + r;
+        var cy = y + r;
+        ctx.arc(cx, cy, r, 0, 2 * Math.PI);
+        ctx.clip();
+        ctx.drawImage(img, x, y, d, d);
+        ctx.restore();
+      }
+
+      function drawRedPacketBg(ctx,w,y) {
+        ctx.beginPath();
+        ctx.setFillStyle("#EB5646");
+        ctx.lineTo(w, 0);
+        ctx.lineTo(w, y);
+        ctx.arc(w / 2, y, w / 2, 0, Math.PI, false);
+        ctx.lineTo(0, y);
+        ctx.lineTo(0, 0);
+        ctx.closePath();
+        ctx.fill();
+      }
+
+      function drawRedPacketText(ctx, text, font,w,y, isMoney) {
+        ctx.font = 'normal bold ' + font + 'px sans-serif'
         ctx.setFillStyle('#FDE1B1') //字体颜色
         ctx.textAlign = "center"; //文字居中
-        ctx.fillText('元',x + (text.length/2)*30,y);
+        ctx.fillText(text,w/2, y);
+        if (isMoney) {
+          ctx.setFontSize(13) //字体大小
+          ctx.setFillStyle('#FDE1B1') //字体颜色
+          ctx.textAlign = "center"; //文字居中
+          ctx.fillText('元', w / 2 + (text.length / 2) * 30, y);
+        }
       }
-    },
 
-    _drawRedPacketMove: function (y) {
       var that = this;
       var context2 = wx.createCanvasContext('redPacketCanvas2', that);
       context2.setGlobalAlpha(that.data.transparency);
-      that._drawRedPacketBg(context2,y);
-      that._circleImg(context2,'../../images/me/user_icon_default.png');
-      that._drawRedPacketText(context2,'张三丰的红包',18,70,false);
-      that._drawRedPacketText(context2,'恭喜发财,大吉大利',13,95,false);
-      that._drawRedPacketText(context2,'0.50',50,160,true);
-      that._drawRedPacketText(context2,'已存入钱包',18,200,false);
+      var w = that.data.windowWidth;
+      drawRedPacketBg(context2,w,y);
+      circleImg(context2,w,that.data.iconUrl);
+      drawRedPacketText(context2, that.data.userName + '的红包', 18, w,70,false);
+      drawRedPacketText(context2, that.data.message, 13, w,95,false);
+      drawRedPacketText(context2, that.data.money.toFixed(2), 50, w,160,true);
+      drawRedPacketText(context2,'已存入钱包',18,w,200,false);
       context2.draw();
     },
 
-    _drawRedPacketOpen:function () {
-      function roundRect(ctx, x, y, w, h, r) {
-        ctx.beginPath()
-        ctx.setFillStyle('#F24E4D')
-        ctx.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5)
-        ctx.moveTo(x + r, y)
-        ctx.lineTo(x + w - r, y)
-        ctx.lineTo(x + w, y + r)
-        ctx.arc(x + w - r, y + r, r, Math.PI * 1.5, Math.PI * 2)
-        ctx.lineTo(x + w, y + h - r)
-        ctx.lineTo(x + w - r, y + h)
-        ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * 0.5)
-        ctx.lineTo(x + r, y + h)
-        ctx.lineTo(x, y + h - r)
-        ctx.arc(x + r, y + h - r, r, Math.PI * 0.5, Math.PI)
-        ctx.lineTo(x, y + r)
-        ctx.lineTo(x + r, y)
-        ctx.fill()
-        ctx.closePath()
-      }
+      _roundRect: function(ctx, x, y, w, h, r) {
+          ctx.beginPath()
+          ctx.setFillStyle('#F24E4D')
+          ctx.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5)
+          ctx.moveTo(x + r, y)
+          ctx.lineTo(x + w - r, y)
+          ctx.lineTo(x + w, y + r)
+          ctx.arc(x + w - r, y + r, r, Math.PI * 1.5, Math.PI * 2)
+          ctx.lineTo(x + w, y + h - r)
+          ctx.lineTo(x + w - r, y + h)
+          ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * 0.5)
+          ctx.lineTo(x + r, y + h)
+          ctx.lineTo(x, y + h - r)
+          ctx.arc(x + r, y + h - r, r, Math.PI * 0.5, Math.PI)
+          ctx.lineTo(x, y + r)
+          ctx.lineTo(x + r, y)
+          ctx.fill()
+          ctx.closePath()
+    },
 
+    _drawRedPacketOpen:function () {
       var that = this;
       var context1 = wx.createCanvasContext('redPacketCanvas1', that);
+      var w = that.data.windowWidth;
+      var h = that.data.windowHeight;
+
       if (that.data.coinHidden) {
-        var height = that.data.windowHeight * 0.8;
-        var width  = that.data.windowWidth * 0.8;
+        var height = h * 0.8;
+        var width  = w * 0.8;
         context1.save();
-        roundRect(context1,0,that.data.move_y1, width, height/2,0);
+        that._roundRect(context1,0,that.data.moveY1, width, height/2,0);
         context1.restore();
 
         context1.save();
-        roundRect(context1,0,that.data.move_y2, width, height/2, 0);
+        that._roundRect(context1,0,that.data.moveY2, width, height/2, 0);
         context1.restore();
       }else {
         context1.save();
-        var y = that.data.windowHeight * 0.8 * 0.1;
-        var height = that.data.windowHeight * 0.8;
-        var width = that.data.windowWidth * 0.8;
-        roundRect(context1, 0, y, width, height,10);
+        var y = h * 0.1;
+        var height = h * 0.8;
+        var width  = w * 0.8;
+        that._roundRect(context1, 0, y, width, height,10);
         context1.restore();
 
         context1.save();
         context1.beginPath()
         context1.setFillStyle("#F24E4D");
         context1.setShadow(2,2,2,"#D23D3C");
-        var x = that.data.windowWidth * 0.8 / 2;
-        var r = that.data.windowWidth * 0.8 / 2;
+        var x = w * 0.8 / 2;
+        var r = w * 0.8 / 2;
         context1.arc(x,y+2,r-4,0, Math.PI, false)
         context1.fill();
         context1.closePath()
         context1.restore();
 
         context1.save();
-        var moneyImage = that.data.moneyImages[that.data.current_i];
+        var moneyImage = that.data.moneyImages[that.data.index];
         var imageWidth = moneyImage.width * (100 / moneyImage.height);
         var imageHeight = 100;
-        var money_x = (that.data.windowWidth * 0.8 - imageWidth) / 2;
-        var money_y = (that.data.windowHeight - imageHeight)/ 2;
+        var money_x = (w * 0.8 - imageWidth) / 2;
+        var money_y = (h - imageHeight)/ 2;
         context1.drawImage(moneyImage.url, money_x, money_y, imageWidth, imageHeight);
         context1.restore();
+
       }
       context1.draw();
     },
 
-    _canvasStart: function (e) {
+    _drawRedPacketStart:function (scale) {
+      var that = this;
+      var context1 = wx.createCanvasContext('redPacketCanvas1', that);
+      var w = that.data.windowWidth;
+      var h = that.data.windowHeight;
+      context1.save();
+      var w1 = w * 0.8 * scale;
+      var h1 = h * 0.8 * scale;
+      var x = (w * 0.8 - w1)/2;
+      var y = (h - h1)/2;
+      that._roundRect(context1,x,y,w1,h1, 10);
+      context1.restore();
+
+      context1.save();
+      context1.beginPath()
+      context1.setFillStyle("#F24E4D");
+      context1.setShadow(2, 2, 2, "#D23D3C");
+      var x = w * 0.8 / 2;
+      var r = (w * 0.8 / 2) * scale;
+      context1.arc(x, y + 2, r - 4, 0, Math.PI, false)
+      context1.fill();
+      context1.closePath()
+      context1.restore();
+
+      context1.draw();
     },
+
+    _canvasStart: function (e) {
+      var that = this;
+    },
+
     _canvasMove: function (e) {
       var that = this;
       that.setData({
-        current_y: that.data.current_y + 2
+        startY: that.data.startY + 2
       });
-      this._drawRedPacketMove(that.data.current_y)
+      this._drawRedPacketEnd(that.data.startY)
     },
+    
     _canvasEnd: function (e) {
       var that = this;
       that.setData({
-        current_y: 100
+        startY: 100
       });
-      this._drawRedPacketMove(that.data.current_y)
+      this._drawRedPacketEnd(that.data.startY)
     }
   },
 
   attached: function (e) {
-    // that._drawRedPacketMove(that.data.current_y);
+    
   },
    
   ready: function () { 
-    var that = this;
-    that._drawRedPacketOpen();
-    that.data.setInter = setInterval(
-      function () {
-        that.setData({
-          current_i: that.data.current_i >= 10 ? 0 : (that.data.current_i + 1),
-        });
-        if(that.data.coinHidden) {
-          that.setData({
-            move_y1: that.data.move_y1 - 60,
-            move_y2: that.data.move_y2 + 60,
-            transparency: that.data.transparency + 0.1
-          });
-          that._drawRedPacketMove(that.data.current_y);
-          if (that.data.move_y1 < - (that.data.windowHeight * 0.4 + 60)
-            && that.data.move_y2 > that.data.windowHeight + 60) {
-            clearInterval(that.data.setInter);
-            that.setData({
-              transparency: 1
-            });
-            that._drawRedPacketMove(that.data.current_y);
-          }
-        }
-        that._drawRedPacketOpen();
-      }, 100);  
-     
-    that.data.currentTimeout1 = setTimeout(function () {
-      that.setData({
-        coinHidden:true
-      })  
-    }, 5000);
+
   },
 
 
@@ -277,7 +368,7 @@ Component({
   },
   
   detached: function () { 
-    clearTimeout(this.data.currentTimeout1);
+    clearTimeout(this.data.currentTimeout);
     clearInterval(this.data.setInter);
   },
 
