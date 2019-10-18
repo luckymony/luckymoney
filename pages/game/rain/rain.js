27195b3031e59c5258e29138085f65961527424e
550476128@qq.com <Hdy!13579>
2019-10-18 23:11:39 +0800

    基本完善红包雨控制逻辑

--- pages/game/rain/rain.js
+++ pages/game/rain/rain.js
@@ -11,6 +11,9 @@ Page({
     time:10,
     addRedpackTime:null,
     dreawTime:null,
+    animationData:null,
+    score:0,
+    changeScore:0,
     redpackets:[]
   },
 
@@ -59,12 +62,18 @@ Page({
         if (that.data.redpackets.length > 0){
           lastRedpacket = that.data.redpackets[that.data.redpackets.length - 1];
         }
+        var type = that.randInt(1,4);
+        var score = type <= 2 ? that.randInt(0, 10) : that.randInt(-10,-1);
+        var src1 = '../../../images/rain/rdc.png';
+        var src2 = '../../../images/rain/zhadan.png';
         if (lastRedpacket === null) {
           var redpacket = {
             w: 64 * that.rand(0.8, 1.0),
             y: 0,
             x: that.rand(0, windowWidth- 64 * that.rand(0.8, 1.0)),
-            image:'../../../images/rain/rdc.png'
+            image: type <= 2 ? src1 : src2,
+            type: type,
+            score: score
           }
           that.data.redpackets.push(redpacket)
         }else {
@@ -73,7 +82,9 @@ Page({
               w: 64 * that.rand(0.8, 1.0),
               y: 0,
               x: that.rand(0, windowWidth/2),
-              image: '../../../images/rain/rdc.png'
+              image: type <= 2 ? src1 : src2,
+              type: type,
+              score: score
             }
             that.data.redpackets.push(redpacket)
           }else {
@@ -81,12 +92,14 @@ Page({
               w: 64 * that.rand(0.8, 1.0),
               y: 0,
               x: that.rand(windowWidth / 2, windowWidth - 64 * that.rand(0.8, 1.0)),
-              image: '../../../images/rain/zhadan.png'
+              image: type <= 2 ? src1 : src2,
+              type: type,
+              score: score
             }
             that.data.redpackets.push(redpacket)
           }
         }
-         console.log(that.data.redpackets);
+        //  console.log(that.data.redpackets);
     },350)
     that.redpacketRolling();
   },
@@ -126,6 +139,10 @@ Page({
     return parseFloat(Math.random() * (max - min) + min);
   },
 
+  randInt(min,max) {
+    return parseInt(Math.random() * (max - min) + min);
+  },
+
   /**
    * 生命周期函数--监听页面卸载
    */
@@ -133,6 +150,55 @@ Page({
     clearInterval(this.data.dreawTime);
     clearInterval(this.data.gameTimer);
     clearInterval(this.data.addRedpackTime);
+  },
+
+  touchStart: function (evt) {
+    var that = this;
+    //console.log(evt.touches[0].x, evt.touches[0].y);
+    for (var i = 0; i < that.data.redpackets.length; i++) {
+      var redpacket = that.data.redpackets[i];
+      if (redpacket.x + redpacket.w >= evt.touches[0].x
+        && redpacket.x <= evt.touches[0].x
+        && redpacket.y + redpacket.w >= evt.touches[0].y
+        && redpacket.y <= evt.touches[0].y
+        ){
+            that.animationOfScore();
+            // console.log('点中了'+i+'红包');
+            that.data.redpackets.splice(i, 1)
+            var score = that.data.score + redpacket.score;
+            that.setData ({
+              changeScore:redpacket.score,
+              score: score
+            })
+        }
+    }
+  },
+
+  touchMove: function (evt) {
+    
+  },
+
+  touchEnd: function (evt) {
+   
+  },
+
+  //分数动画
+  animationOfScore() {
+    const animation = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'ease'
+    })
+    this.animation = animation
+    animation.opacity(1).step()
+    setTimeout(
+      function () {
+        animation.opacity(0).step()
+        this.setData({
+          animationData: animation.export()
+        })
+      }.bind(this),
+      10
+    )
   },
 
 });
