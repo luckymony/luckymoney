4f8a9b92a10f040a67747043c8d58cf1d91500cd
hedongyang <hedy@idoosmart.com>
2020-01-17 18:23:45 +0800

    优化界面

---
+++ component/paymoney/paymoney.js
@@ -0,0 +1,31 @@
+// component/paymoney/paymoney.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    //祝福语
+    luckyStr: {
+      type: String,
+      value: '恭喜发财 大吉大利'
+    },
+    money: {
+      type:Number,
+      value:null
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
